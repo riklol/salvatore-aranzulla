3bdb330b86ff8058459d4e5a4582492d63d703df
DanyB0 <daniele.beltrami13@gmail.com>
2022-01-15 20:42:06 +0100

    Add credits

---
+++ cogs/credits.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+
+
+class Credits(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="credits")
+    async def credits(self, ctx: commands.Context):
+        """Send the credits."""
+        danyb0, alesar = get_emojis()
+        await message.channel.send(
+            f"> **Thanks to:**\n> DanyB0#6791 {danyb0}\n> alesar03#4718 {alesar}"
+        )
+
+
+def get_emojis():
+    danyb0 = client.get_emoji(850099185993777179)
+    alesar = client.get_emoji(850099186003083304)
+    return danyb0, alesar
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Credits(bot))
