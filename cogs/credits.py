b67e4ff9879d9dd828d37545c0cfe659338af9c4
DanyB0 <daniele.beltrami13@gmail.com>
2022-03-01 22:55:28 +0100

    Update messages style

--- cogs/credits.py
+++ cogs/credits.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
@@ -11,7 +12,14 @@ class Credits(commands.Cog):
     async def credits(self, ctx: commands.Context):
         """Show the credits."""
         danyb0 = "<a:whyyyyyy:850099185993777179>"
-        await ctx.send(f"> **Thanks to:**\n> DanyB0#6791  {danyb0}\n> alesar03#4718")
+        embed = discord.Embed(
+            title="Thanks to",
+            url="https://github.com/DanyB0/salvatore-aranzulla",
+            color=discord.Color.blue(),
+        )
+        embed.add_field(name="DanyB0#6791", value="lol")
+        embed.add_field(name="alesar03#4718", value="lmao")
+        await ctx.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
