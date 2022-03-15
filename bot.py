3eedd28ac378e2a806eb89efc3d9c260e5ea9aae
DanyB0 <daniele.beltrami13@gmail.com>
2022-03-15 17:08:46 +0100

    Add message when bot is online

--- bot.py
+++ bot.py
@@ -13,6 +13,13 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+@bot.event
+async def on_ready():
+    # send a message on the console and in a specific channel when the bot is online
+    channel = bot.get_channel(941372997674106951)
+    print("\nSalvatore è online!\n")
+    await channel.send("\nSono online!")
+
 # load the functions
 bot.load_extension("cogs.credits")
 bot.load_extension("cogs.ping")
