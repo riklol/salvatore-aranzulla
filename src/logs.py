c36c1a4c794878ca650a9a918515cfb473d720df
DanyB0 <daniele.beltrami13@gmail.com>
2022-04-23 00:54:38 +0200

    Add logs file

---
+++ src/logs.py
@@ -0,0 +1,10 @@
+"""This file handle all the logs"""
+
+import datetime
+
+import src
+
+date = datetime.datetime.now().strftime("%Y-%m-%d")
+hour = datetime.datetime.now().strftime("%H:%M:%S")
+
+# AGGIUNGERE I LOG
