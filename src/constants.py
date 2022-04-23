807b9ad659724950fcd7ed94d2dc0e13e6081b03
DanyB0 <daniele.beltrami13@gmail.com>
2022-04-23 13:34:56 +0200

    Moved constants here

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,17 @@ from os import getenv
 
 from dotenv import load_dotenv
 
+import datetime
+
+# bot token
 load_dotenv()
 TOKEN = getenv("TOKEN")
 
+# base directory
 BASE_DIR = os.getcwd()
+
+# these are not constants but are
+# here to simplify the work
+date = datetime.datetime.now().strftime("%Y-%m-%d")
+hour = datetime.datetime.now().strftime("%H:%M:%S")
+
