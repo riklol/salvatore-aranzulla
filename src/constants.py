40c78d47d9c5135c3c0245c646ce64e668eac074
DanyB0 <daniele.beltrami13@gmail.com>
2022-04-23 04:35:41 -0700

    Fix typo

--- src/constants.py
+++ src/constants.py
@@ -14,8 +14,7 @@ TOKEN = getenv("TOKEN")
 # base directory
 BASE_DIR = os.getcwd()
 
-# these are not constants but are
-# here to simplify the work
+# these are not constants but are here to simplify the work
 date = datetime.datetime.now().strftime("%Y-%m-%d")
 hour = datetime.datetime.now().strftime("%H:%M:%S")
 
