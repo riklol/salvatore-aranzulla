983ea4b91ce961bee076872dd1a4b9a8141c46c6
alesar1 <54070865+alesar1@users.noreply.github.com>
2023-02-17 18:14:46 +0100

    move to yt_dlp

--- cogs/vocal.py
+++ cogs/vocal.py
@@ -7,7 +7,7 @@ import requests
 from discord import FFmpegPCMAudio
 from discord.ext import commands
 from discord.utils import get
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 import utils
 
